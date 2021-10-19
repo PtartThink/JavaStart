33afac07707a0606c0b455c80c26eb23b877b650
PifeThink <38389223+PifeThink@users.noreply.github.com>
2021-10-19 15:46:11 +0800

    更改

--- Student Manage System/src/system/StudentDao.java
+++ Student Manage System/src/system/StudentDao.java
@@ -1,4 +1,34 @@
 package system;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class StudentDao {
+    List<Student> studentsList = new ArrayList<Student>();
+    Scanner input = new Scanner(System.in);
+
+    //返回所有学生信息。
+    public List<Student> getAllStudent(){
+        return studentsList;
+    }
+
+    //学生添加
+    public void addStudent(List<Student> stu){
+        studentsList.add((Student) stu);
+
+    }
+
+    //根据学号查询学生
+
+    public Student getStudentByID(int id){
+        for(int i = 0;i<studentsList.size();i++){
+            if (studentsList.get(i)!=null&& (studentsList.get(i).getId())==id){
+                return studentsList.get(i);
+            }
+        }
+        return null;
+    }
+
+
 }
--- Student Manage System/src/system/Team.java
+++ Student Manage System/src/system/Team.java
@@ -1,4 +1,11 @@
 package system;
 
+import java.util.Scanner;
+
 public class Team {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
 }
